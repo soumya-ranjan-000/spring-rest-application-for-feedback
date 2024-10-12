8fcd293c9bec33cd6b2d51ce03632c83c12f85c1
soumya-ranjan-000 <58003666+soumya-ranjan-000@users.noreply.github.com>
2024-10-12 13:25:28 +0000

    added java file

---
+++ src/main/java/com/srg/caserattings/CaserattingsApplication.java
@@ -0,0 +1,13 @@
+package com.srg.caserattings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaserattingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaserattingsApplication.class, args);
+	}
+
+}
